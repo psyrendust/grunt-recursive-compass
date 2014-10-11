a265dddb841957c618f2da3dc600ee554e65f39a
Larry Gordon <lgordon@psyrendust.com>
2014-10-11 12:58:33 -0700

    Updates recursive-compass.js to work with Compass 1.x and Grunt 0.4.x

--- tasks/recursive-compass.js
+++ tasks/recursive-compass.js
@@ -1,30 +1,32 @@
-/*
+/**
  * grunt-recursive-compass
- * https://github.com/psyrendust/grunt-recursive-compass
+ * https://github.com/psyrendust/grunt-recursive-compass/
  *
- * Copyright (c) 2013 Larry Gordon
- * Licensed under the MIT license.
+ * Copyright (c) 2014 Larry Gordon
+ * http://psyrendust.mit-license.org/2014/license.html
  */
 
 'use strict';
 
 module.exports = function(grunt) {
 
-  var _       = require('underscore');
-  var fs      = require('fs');
-  var path    = require('path');
-  var helpers = require('grunt-lib-contrib').init(grunt);
-  var cmd     = process.platform === 'win32' ? 'compass.bat' : 'compass';
+  var cmd = process.platform === 'win32' ? 'compass.bat' : 'compass';
+  var dargs = require('dargs');
+  var path = require('path');
+  var sassPartialReg = /^_/;
 
   function compile(args, cb) {
     var child = grunt.util.spawn({
       cmd: cmd,
       args: args
-    }, function(err, result, code){
+    }, function(err, result, code) {
+      if (err) {
+        grunt.log.warn(err);
+      }
       cb((code === 0 || !/Nothing to compile/g.test(result.stdout)) || result.stderr);
     }).on('exit', function(code){
       if (code === 127) {
-        grunt.warn(
+        grunt.log.warn(
           'You need to have Ruby and Compass installed ' +
           'and in your system PATH for this task to work. ' +
           'More info: https://github.com/psyrendust/grunt-recursive-compass'
@@ -35,53 +37,32 @@ module.exports = function(grunt) {
     child.stderr.pipe(process.stderr);
   }
 
-  function convertFilesToArray(files) {
-    if( _.isString(files) ) {
-      return [files];
-    } else if ( _.isArray(files) ) {
-      return files;
-    }
-    // It's neither a String or Array.
-    // Return an empty Array
-    return [];
-  }
-
   grunt.registerMultiTask('recursive-compass', 'Recursively compile Compass to CSS.', function() {
-    var data = this.data,
-        options = helpers.options(this),
-        cb = this.async(),
-        filesAry = convertFilesToArray(data.files),
-        filesToExclude = convertFilesToArray(data.exclude),
-        filesToProcess = [],
-        foldersHash = {},
-        myArgs = ['compile'].concat(helpers.optsToArgs(options));
+    var done = this.async();
+    var options = this.options();
+    var filesToProcess = [];
+    var myArgs = ['compile'].concat(dargs(options));
 
-    filesToExclude = grunt.file.expandFiles(filesToExclude);
-    filesAry = grunt.file.expandFiles(filesAry);
-
-    // Compass doesn't have a long flag for --import-path
-    if (options.importPath) {
-      myArgs = myArgs.map(function(el) {
-        return el.replace('--import-path', '-I');
-      });
-    }
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      filesToProcess = filesToProcess.concat(f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        }
+        if (grunt.file.isFile(filepath)) {
+          return !sassPartialReg.test(path.basename(filepath));
+        }
+        return false;
+      }));
+    });
 
     grunt.verbose.writeflags(options, 'Options');
 
-    filesAry.forEach(function(file){
-      // Do not process if it's a SASS Partial
-      if(path.basename(file).match(/^_/)) {
-        return;
-      }
-      filesToProcess.push(file);
-    });
-
-    // Process file exclusions
-    filesToProcess = _.difference(filesToProcess, filesToExclude);
-
     myArgs = myArgs.concat(filesToProcess);
 
-    compile(myArgs, cb);
+    compile(myArgs, done);
   });
 
 };
